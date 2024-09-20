02225f985e8c9e12f893bfa0d1a7d826ee7f39fe
SyedAzherAli <syedazherali01@gmail.com>
2024-09-20 07:35:50 +0530

    added nginx, gunicorn config files

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Go to the application directory
+cd /home/ubuntu/onlineshop
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
+venv/bin/pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Gunicron config
+sudo cp -r /home/ubuntu/django-app/scripts/gunicorn.socket /etc/systemd/system/gunicorn.service
+sudo cp -r /home/ubuntu/django-app/scripts/gunicorn.service /etc/systemd/system/gunicorn.service
+sudo cp -r /home/ubuntu/django-app/scripts/gunicron.socket /etc/systemd/system/gunicorn.socket
+sudo systemctl enable gunicorn
+
+# Nginx config
+sudo cp -r /home/ubuntu/django-app/scripts/default /etc/nginx/sites-availabe/default
+sudo gpasswd -a www-data ubuntu
+
+# Restart Gunicorn and nginx
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
